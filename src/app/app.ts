2625f93f09e39e4a51a7d0ddc1e1d8dc2e0246dd
fariamarcele12-a11y <fariamarcele12@gmail.com>
2026-06-02 20:59:27 -0300

    Adicionando o componente product card

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductCard } from './shared/components/product-card/product-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/shared/components/product-card/product-card.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.scss',
+})
+export class ProductCard {
+  title = 'Cachorro';
+  description = 'Adicione a descrição aqui.';
+}
